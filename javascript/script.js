dad8daa2893a27d8e5fb58149e240c1c2a7c6604
Fred Dupret <fred.dupret@outlook.com>
2023-06-17 15:36:18 -0300

    areas atuação hover

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+// const galeria = document.querySelectorAll(".areas__galeria__img");
+
+// /** @param {MouseEvent} event */
+// function ativar(event) {
+//   const before = document.querySelector("before");
+//   console.log(before);
+//   elemento = event.currentTarget;
+//   elemento.style.height = 100 + "px";
+// }
+
+// galeria[0].addEventListener("click", ativar);
+
+// window.screenY;
+// window.addEventListener("scroll", scroll);
